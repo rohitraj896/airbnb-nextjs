623e169c06c393d4ebb17fb67867b70a786dcd77
Rohit Kumar <rohitkr3390@gmail.com>
2024-08-24 19:25:47 +0530

    header,dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
-  return <div className="text-3xl">Home page</div>;
+  return (
+    <div>
+      <div className="text-3xl">Home page</div>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        Click me
+      </Button>
+    </div>
+  );
 };
 
 export default HomePage;
