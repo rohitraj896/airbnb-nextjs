ddd0f001f87877bf3f1655385276c9951257aa88
Rohit Kumar <rohitkr3390@gmail.com>
2025-01-26 15:39:49 +0530

    Signout

--- components/navbar/SignOut.tsx
+++ components/navbar/SignOut.tsx
@@ -1,5 +1,19 @@
+"use client";
+import { SignOutButton } from "@clerk/nextjs";
+import { useToast } from "../ui/use-toast";
+
 const SignOut = () => {
-  return <div>SignOut</div>;
+  const { toast } = useToast();
+  const handleLogout = () => {
+    toast({ description: "You have been sign out." });
+  };
+  return (
+    <SignOutButton redirectUrl="/">
+      <button className="w-full text-left " onClick={handleLogout}>
+        Logout
+      </button>
+    </SignOutButton>
+  );
 };
 
 export default SignOut;
